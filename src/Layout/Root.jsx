86246f910dc1f4b1d2d6b4101e6e0a220f45ae7e
Asadulla56 <mdasadullahoque294@gmail.com>
2025-05-27 02:05:44 +0600

    Navbar done

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navber from '../Components/Share/Navber';
 
 const Root = () => {
     return (
         <div>
-            <h1>hi</h1>
+            <Navber></Navber>
             <Outlet/>
+
         </div>
     );
 };
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-             <h1 className='text-center text-red-300'>this is home section</h1>
+            
         </div>
     );
 };
