45e7550e1926600b55c9a828d0c7184d138c8ad2
mubashirbappi <mubashirbappi@gmail.com>
2025-05-29 20:41:24 +0600

    implement hero section

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Gallery from './Gallery';
 import Testimonials from '../Components/Home/Testimonials';
+import Hero from '../Components/Home/Hero';
 
 const Home = () => {
     return (
         <div>
+            <Hero/>
             <Testimonials/>
             <Gallery></Gallery>
         </div>
