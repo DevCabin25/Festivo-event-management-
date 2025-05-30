f6a9580f6948f8e3133bd9c563d9d2d8af22881e
mubashirbappi <mubashirbappi@gmail.com>
2025-05-30 21:56:18 +0600

    update service section

--- src/Components/Share/Navber.jsx
+++ src/Components/Share/Navber.jsx
@@ -33,7 +33,7 @@ const Navber = () => {
                                 Home
                             </Link>
                             <Link to="/events" className="text-gray-950 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200">
-                                Events
+                                Services
                             </Link>
                             <Link to="/about" className="text-gray-950 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200">
                                 About
